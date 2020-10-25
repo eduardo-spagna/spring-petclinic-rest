9bfe5ff33ba0746ef31e5c162599d0b858e3f539
Eduardo Spagna <eduardo@gempe.dev>
2020-10-25 03:13:35 -0300

    Atribui valor para a flag enabled ao atualizar um proprietário

--- src/main/java/org/springframework/samples/petclinic/rest/OwnerRestController.java
+++ src/main/java/org/springframework/samples/petclinic/rest/OwnerRestController.java
@@ -121,7 +121,8 @@ public class OwnerRestController {
 		currentOwner.setCity(owner.getCity());
 		currentOwner.setFirstName(owner.getFirstName());
 		currentOwner.setLastName(owner.getLastName());
-		currentOwner.setTelephone(owner.getTelephone());
+        currentOwner.setTelephone(owner.getTelephone());
+        currentOwner.setEnabled(owner.isEnabled());
 		this.clinicService.saveOwner(currentOwner);
 		return new ResponseEntity<Owner>(currentOwner, HttpStatus.NO_CONTENT);
 	}
